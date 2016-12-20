35359eb4ced763b55875813982a59155b785df34
mdietz <mlewisdietz@gmail.com>
2016-12-20 14:42:51 -0500

    Added Comic Books List view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -61,6 +61,10 @@ namespace ComicBookGallery.Data
             }
         };
 
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
